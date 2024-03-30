8a4fd85c148d581288fa09449338ea61070b3991
Anton Akuev <antonakuev945@mail.ru>
2024-03-30 13:26:24 +0300

    fix: Поменял вызов класса

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ import asyncio
 from datetime import datetime
 
 from src.config.file_env import settings
+from src.services.count_service import CountNewsService
+from src.services.parser_service import ParserNewsService
+from src.services.record_service import RecordNewsService
 from src.start import RssParser
 
 
 async def get_rss_parser() -> None:
     """Получить класс операций."""
     while True:
-        one = datetime.now()
-        await RssParser.execute_activities()
+        await RssParser(
+            ParserNewsService, RecordNewsService, CountNewsService
+        ).execute_activities(settings.rss_link)
         await asyncio.sleep(settings.time_sleep)
-        print(datetime.now() - one)
 
 if __name__ == "__main__":
     asyncio.run(get_rss_parser())
